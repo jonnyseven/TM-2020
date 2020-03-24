dba8fd5289a371fef5c8b6ccbabef9fa519d359a
jonnyseven <50323657+jonnyseven@users.noreply.github.com>
2020-03-24 07:20:50 +0000

    esquecimento

---
+++ Aula 2/exercicio2/joaopereira-18471/script.js
@@ -0,0 +1,22 @@
+var arrayNumbers = [];
+
+
+function adicionarNumero(){
+   var numero = document.getElementById("numbersId").value;
+
+   arrayNumbers.push(parseInt(numero));
+
+
+   document.getElementById("numerosAdicionados").innerText= arrayNumbers.toString();
+   console.log(arrayNumbers);
+}
+
+function calcularNumero() {
+
+
+    var maiorNumero = Math.max.apply(Math,arrayNumbers);
+    if(arrayNumbers.length>5){
+    alert(maiorNumero);
+    }
+
+}
